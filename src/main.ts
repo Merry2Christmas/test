10edc6defca1b41dff86a4a5d155e05276cfd37c
boilingSugar <1418068612@qq.com>
2026-02-02 11:22:03 +0800

    feat 引入arco和pinia，并在main.ts 挂载

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import { createApp } from "vue";
+import ArcoVue from "@arco-design/web-vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import "@arco-design/web-vue/dist/arco.css";
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(ArcoVue);
+app.mount("#app");
